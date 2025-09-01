7db650400aef71265a38853611809b99e0ac7747
Kritik Paudel <kritikpaudel69@gmail.com>
2025-09-01 23:08:22 +0545

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+// Use Vite's base to derive the Router basename (drop trailing slash)
+const basename = (import.meta.env.BASE_URL || "/").replace(/\/$/, "");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="https://kritikpaudel.github.io/portfolio/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
